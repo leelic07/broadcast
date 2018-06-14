001b2f0b57522ddcfb97bf749f9119b91a56fbc0
leelic07 <330314702@qq.com>
2018-06-14 15:25:28 +0800

    整理组件格式

--- src/views/broadcast/views/edit.vue
+++ src/views/broadcast/views/edit.vue
@@ -1,15 +1,19 @@
 <template>
   <el-row class="broadcast-edit-container">
-    <el-col :span="16" :offset="4">
+    <el-col :span="16"
+      :offset="4">
       <el-card>
         <el-col>
-          <el-button type="text" icon="el-icon-back" @click="back">返回</el-button>
+          <el-button type="text"
+            icon="el-icon-back"
+            @click="back">返回</el-button>
           <h2>
             <i class="el-icon-edit"></i>
             编辑记录
           </h2>
         </el-col>
-        <el-form label-position="top" :model="broadForEdit">
+        <el-form label-position="top"
+          :model="broadForEdit">
           <el-form-item>
             <el-tag>
               <i class="el-icon-time"></i>
@@ -23,16 +27,21 @@
               播放
             </el-tag>
             <el-col v-if="broadForEdit.filename">
-              <audio :src="$_baseURL + broadForEdit.filename" controls>
+              <audio :src="$_baseURL + broadForEdit.filename"
+                controls>
                 您的浏览器不支持播放音频文件
               </audio>
             </el-col>
-            <el-col v-else v-for="(file,index) in broadForEdit.refs" :key="index">
+            <el-col v-else
+              v-for="(file,index) in broadForEdit.refs"
+              :key="index">
               <el-col :span="22">
-                <audio :src="file.filename" controls></audio>
+                <audio :src="file.filename"
+                  controls></audio>
               </el-col>
               <el-col :span="2">
-                <el-button type="text" @click="showDetail(file.id)">详情</el-button>
+                <el-button type="text"
+                  @click="showDetail(file.id)">详情</el-button>
               </el-col>
             </el-col>
           </el-form-item>
@@ -42,14 +51,29 @@
               内容
             </el-tag>
             <p v-if="!isModification">{{broadForEdit.content || '暂无内容'}}</p>
-            <el-input v-if="isModification" type="textarea" v-model="broadForEdit.content" size="meduim" :autosize="{minRows: 4,maxRows: 35}" placeholder="请输入内容"></el-input>
-            <el-button type="warning" plain size="mini" v-if="isModification" icon="el-icon-refresh" @click="cancel">取消</el-button>
-            <el-button size="mini" type="primary" plain icon="el-icon-edit" @click="modify">{{isModification? '修改': '编辑'}}</el-button>
+            <el-input v-if="isModification"
+              type="textarea"
+              v-model="broadForEdit.content"
+              size="meduim"
+              :autosize="{minRows: 4,maxRows: 35}"
+              placeholder="请输入内容"></el-input>
+            <el-button type="warning"
+              plain
+              size="mini"
+              v-if="isModification"
+              icon="el-icon-refresh"
+              @click="cancel">取消</el-button>
+            <el-button size="mini"
+              type="primary"
+              plain
+              icon="el-icon-edit"
+              @click="modify">{{isModification? '修改': '编辑'}}</el-button>
           </el-form-item>
         </el-form>
       </el-card>
     </el-col>
-    <el-dialog title="记录详情" :visible.sync="dialogFormVisible">
+    <el-dialog title="记录详情"
+      :visible.sync="dialogFormVisible">
       <el-form :model="recordingDetail">
         <el-form-item :label-width="formLabelWidth">
           <el-tag>
@@ -66,8 +90,11 @@
           <p>{{recordingDetail.content || '暂无内容'}}</p>
         </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogFormVisible = false" size="small">确 定</el-button>
+      <div slot="footer"
+        class="dialog-footer">
+        <el-button type="primary"
+          @click="dialogFormVisible = false"
+          size="small">确 定</el-button>
       </div>
     </el-dialog>
   </el-row>
@@ -79,7 +106,6 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data () {
     return {
-      edit: 'BroadcastEdit',
       isModification: false,
       dialogFormVisible: false,
       formLabelWidth: '80px'
--- src/views/broadcast/views/merge.vue
+++ src/views/broadcast/views/merge.vue
@@ -1,47 +1,57 @@
 <template>
-    <el-row class="merge-container">
-        <el-col :span="16" :offset="4">
-            <el-card>
-                <el-col>
-                    <el-button type="text" icon="el-icon-back" @click="back">返回</el-button>
-                    <h2>
-                        <i class="el-icon-menu"></i>
-                        合并记录
-                    </h2>
-                </el-col>
-                <el-form>
-                    <el-form-item v-for="(record,index) in checkedRecording" :key="index">
-                        <el-tag type="success">
-                            <i class="el-icon-service"></i>
-                            {{record.timestamp | Date}}
-                        </el-tag>
-                        <p class="broadcast-content">{{record.content || '暂无记录'}}</p>
-                    </el-form-item>
-                    <el-col>
-                        <el-tag>
-                            <i class="el-icon-document"></i>
-                            合并内容
-                        </el-tag>
-                    </el-col>
-                    <el-form-item>
-                        <el-input type="textarea" v-model="contents" :autosize="{minRows: 4,maxRows: 35}" size="medium" :disabled="true"></el-input>
-                        <!-- <p v-for="(record,index) in checkedRecording" :key="index">
+  <el-row class="merge-container">
+    <el-col :span="16"
+      :offset="4">
+      <el-card>
+        <el-col>
+          <el-button type="text"
+            icon="el-icon-back"
+            @click="back">返回</el-button>
+          <h2>
+            <i class="el-icon-menu"></i>
+            合并记录
+          </h2>
+        </el-col>
+        <el-form>
+          <el-form-item v-for="(record,index) in checkedRecording"
+            :key="index">
+            <el-tag type="success">
+              <i class="el-icon-service"></i>
+              {{record.timestamp | Date}}
+            </el-tag>
+            <p class="broadcast-content">{{record.content || '暂无记录'}}</p>
+          </el-form-item>
+          <el-col>
+            <el-tag>
+              <i class="el-icon-document"></i>
+              合并内容
+            </el-tag>
+          </el-col>
+          <el-form-item>
+            <el-input type="textarea"
+              v-model="contents"
+              :autosize="{minRows: 4,maxRows: 35}"
+              size="medium"
+              :disabled="true"></el-input>
+            <!-- <p v-for="(record,index) in checkedRecording" :key="index">
                         {{record.content}}
                     </p> -->
-                    </el-form-item>
-                    <el-col>
-                        <el-tag>
-                            <i class="el-icon-download"></i>
-                            下载地址
-                        </el-tag>
-                    </el-col>
-                    <el-form-item>
-                        <el-button icon="el-icon-download" type="text" @click="downloadWord">点击下载合并记录</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-card>
-        </el-col>
-    </el-row>
+          </el-form-item>
+          <el-col>
+            <el-tag>
+              <i class="el-icon-download"></i>
+              下载地址
+            </el-tag>
+          </el-col>
+          <el-form-item>
+            <el-button icon="el-icon-download"
+              type="text"
+              @click="downloadWord">点击下载合并记录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
